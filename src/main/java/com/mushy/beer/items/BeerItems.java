ddedffdc9f80925fca747bf1aa998447e5c0744a
forestswag@gmail.com <forestswag@gmail.com>
2020-08-07 23:09:23 -0700

    Added other beer styles and textures

--- src/main/java/com/mushy/beer/items/BeerItems.java
+++ src/main/java/com/mushy/beer/items/BeerItems.java
@@ -13,5 +13,11 @@ public class BeerItems {
    public static final DeferredRegister<Item> BEER_ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Beer.ModId);
 
    public static final RegistryObject<Item> BEER = BEER_ITEMS.register("beer", BeerItem::new);
+   public static final RegistryObject<Item> LAGER = BEER_ITEMS.register("lager", BeerItem::new);
+   public static final RegistryObject<Item> IPA = BEER_ITEMS.register("ipa", BeerItem::new);
+   public static final RegistryObject<Item> AMBER = BEER_ITEMS.register("amber", BeerItem::new);
+   public static final RegistryObject<Item> STOUT = BEER_ITEMS.register("stout", BeerItem::new);
+
+
 
 }
